bfe708fb79d0e22842685349d950ade17774d324
Vo Hoang Long <vohoanglong07@gmail.com>
2020-08-14 17:07:27 +0700

    learn react

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,21 +1,53 @@
-console.log('app is running....')
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.toggleVisibity = this.toggleVisibity.bind(this)
+    this.state = {
+      hidden: true,
+      btnText: 'Show details',
+    }
+  }
 
-let hidden = true
-const toggleVisibity = () => {
-  hidden = !hidden
-  render()
+  toggleVisibity() {
+    this.setState((prevState) => {
+      return {
+        hidden: !prevState.hidden,
+        btnText: prevState.hidden ? 'Hidden details' : 'Show details',
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.toggleVisibity}>{this.state.btnText}</button>
+        {!this.state.hidden ? <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p> : ''}
+      </div>
+    )
+  }
 }
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibity}> {hidden ? 'Show details' : 'Hidden details'}</button>
-      {!hidden ? <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p> : ''}
-    </div>
-  )
-  ReactDOM.render(template, appRoot)
-}
+ReactDOM.render(<VisibilityToggle />, document.querySelector('#app'))
 
-const appRoot = document.querySelector('#app')
-render()
+//console.log('app is running....')
+//
+//let hidden = true
+//const toggleVisibity = () => {
+//  hidden = !hidden
+//  render()
+//}
+//
+//const render = () => {
+//  const template = (
+//    <div>
+//      <h1>Visibility Toggle</h1>
+//      <button onClick={toggleVisibity}> {hidden ? 'Show details' : 'Hidden details'}</button>
+//      {!hidden ? <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p> : ''}
+//    </div>
+//  )
+//  ReactDOM.render(template, appRoot)
+//}
+//
+//const appRoot = document.querySelector('#app')
+//render()
--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -1,38 +1,124 @@
-let count = 0
-const myId = 'myId'
-const myClassName = 'myClassName'
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleAddOne = this.handleAddOne.bind(this)
+    this.handleMinusOne = this.handleMinusOne.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+    this.state = {
+      count: props.count,
+    }
+  }
 
-const addOne = () => {
-  count++
-  renderCounterApp()
+  componentDidMount() {
+    console.log('loaded data')
+    try {
+      const json = localStorage.getItem('count')
+      const count = parseInt(json, 10) || 0
+      this.setState(() => ({ count }))
+    } catch (e) {
+      // nothing
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count)
+      console.log('saved data')
+    }
+  }
+
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      }
+    })
+  }
+
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1,
+      }
+    })
+  }
+
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0,
+      }
+    })
+
+    //this.setstate((prevstate) => {
+    //  return {
+    //    count: prevstate.count + 1,
+    //  }
+    //})
+
+    // Async when setState
+    //this.setState({
+    //  count: 0,
+    //})
+
+    //this.setState({
+    //  count: this.state.count + 1,
+    //})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button> &nbsp;
+        <button onClick={this.handleMinusOne}>-1</button> &nbsp;
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    )
+  }
 }
 
-const minusOne = () => {
-  count--
-  renderCounterApp()
+Counter.defaultProps = {
+  count: 0,
 }
 
-const reset = () => {
-  count = 0
-  renderCounterApp()
-}
+ReactDOM.render(<Counter />, document.querySelector('#app'))
 
-const appRoot = document.querySelector('#app') // get app root element
+// let count = 0
+// const myId = 'myId'
+// const myClassName = 'myClassName'
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button id={myId} className={myClassName} onClick={addOne}>
-        +1
-      </button>
-      &nbsp;
-      <button onClick={minusOne}>-1</button>
-      &nbsp;
-      <button onClick={reset}>Reset</button>
-    </div>
-  )
-  ReactDOM.render(templateTwo, appRoot) // render template to root element
-}
+// const addOne = () => {
+//   count++
+//   renderCounterApp()
+// }
 
-renderCounterApp()
+// const minusOne = () => {
+//   count--
+//   renderCounterApp()
+// }
+
+// const reset = () => {
+//   count = 0
+//   renderCounterApp()
+// }
+
+// const appRoot = document.querySelector('#app') // get app root element
+
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button id={myId} className={myClassName} onClick={addOne}>
+//         +1
+//       </button>
+//       &nbsp;
+//       <button onClick={minusOne}>-1</button>
+//       &nbsp;
+//       <button onClick={reset}>Reset</button>
+//     </div>
+//   )
+//   ReactDOM.render(templateTwo, appRoot) // render template to root element
+// }
+
+// renderCounterApp()
